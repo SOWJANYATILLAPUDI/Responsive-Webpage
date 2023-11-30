03a5cd8e3b58fef93d0a008675343eeab6cc019e
SOWJANYATILLAPUDI <104555675+SOWJANYATILLAPUDI@users.noreply.github.com>
2023-11-30 09:58:08 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Space from './components/Space';
+import Community from './components/Community';
+import Footer from './components/Footer';
+import Cards from './components/cards';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Space/>
+    <Community/>
+    <Footer/>
+    <Cards/>
+    </>
+  );
+}
+
+export default App;
